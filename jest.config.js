fdd6ffb626af24d35a2377a7bcc80c9bc47e8f8a
Shane <shane@freenet.de>
2024-11-01 22:56:01 +0100

    Initial commit for GitHub Pages

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], 
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-};
