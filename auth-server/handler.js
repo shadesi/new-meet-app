9b5a9da3ecc69446b3a2436546901b53d93a3698
shadesi <45621420+shadesi@users.noreply.github.com>
2024-11-02 23:38:57 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar('v3');
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  'https://shadesi.github.io/meet/'
+  'https://shadesi.github.io/new-meet-app/'
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
